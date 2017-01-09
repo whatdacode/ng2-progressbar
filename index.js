bfaa45d1bc45f99bc01d01e8bf81a03c675fedae
MurhafSousli <murhafsousli@gmail.com>
2017-01-09 05:39:19 +0300

    update

--- index.js
+++
@@ -1,2 +0,0 @@
-export * from './progress.module';
-//# sourceMappingURL=index.js.map
--- progress.module.d.ts
+++
@@ -1,5 +0,0 @@
-import { ProgressComponent } from "./components/progress/progress.component";
-import { NgProgressService } from './service/progress.service';
-export declare class NgProgressModule {
-}
-export { NgProgressService, ProgressComponent };
